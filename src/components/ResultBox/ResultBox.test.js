04207e82a2093f6b119488b95f4b1fc24783b8b6
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-30 06:47:55 +0200

    Add component rendering test for ResultBox

--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -1,2 +1,8 @@
 import ResultBox from "./ResultBox";
-describe("Component ResultBox", () => {});
+import { render } from "@testing-library/react";
+
+describe("Component ResultBox", () => {
+  it("should render without crashing", () => {
+    render(<ResultBox from="PLN" to="USD" amount={100} />);
+  });
+});
